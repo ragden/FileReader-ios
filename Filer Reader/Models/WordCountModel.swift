c633af4681d1f51e0a838df611a782a27c4f0a67
edgar paz moreno <epaz@opentrends.net>
2017-10-17 23:35:39 +0200

    added file reader logic

---
+++ Filer Reader/Models/WordCountModel.swift
@@ -0,0 +1,53 @@
+//
+//  WordCountModel.swift
+//  Filer Reader
+//
+//  Created by edgar paz moreno on 17/10/17.
+//  Copyright © 2017 edgar paz moreno. All rights reserved.
+//
+
+import UIKit
+
+class WordCountModel: NSObject {
+    var datasource : [WordCount]
+    
+    
+    override init() {
+        datasource = Array()
+    }
+    func readFromFile() {
+        let fileContent = FIleReaderManager.readFile("inputText")
+        self.processString(fileContent: fileContent)
+    }
+    
+    private func processString(fileContent: String) {
+        let stringArray : [String] = fileContent.components(separatedBy: CharacterSet(charactersIn: " \n"))
+        var positionArray : [String] = Array()
+        datasource.removeAll()
+        
+        for string in stringArray {
+            if (positionArray.contains(string.capitalized)) {
+                let index : NSInteger = positionArray.index(of: string.capitalized)!
+                let word = datasource[index]
+                word.count = word.count + 1
+            }
+            else {
+                let word = WordCount(word: string.capitalized, order: datasource.count)
+                positionArray.append(string.capitalized)
+                datasource.append(word)
+            }
+        }
+    }
+}
+
+class WordCount: NSObject {
+    let word : String
+    var count : NSInteger
+    let order : NSInteger
+    
+    init(word: String, order: NSInteger) {
+        self.word = word
+        self.count = 0
+        self.order = order
+    }
+}
