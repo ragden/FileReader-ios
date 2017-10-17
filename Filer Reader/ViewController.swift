80e43f98eacec0906a1def85b624f0a59761c005
edgar paz moreno <epaz@opentrends.net>
2017-10-17 22:00:49 +0200

    Initial Commit

---
+++ Filer Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filer Reader
+//
+//  Created by edgar paz moreno on 17/10/17.
+//  Copyright © 2017 edgar paz moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Filer ReaderTests/Filer_ReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Filer_ReaderTests.swift
+//  Filer ReaderTests
+//
+//  Created by edgar paz moreno on 17/10/17.
+//  Copyright © 2017 edgar paz moreno. All rights reserved.
+//
+
+import XCTest
+@testable import Filer_Reader
+
+class Filer_ReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
