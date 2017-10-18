59bac2e15dbc00645e7c60a307b0701078a48600
Edgar Paz <epaz@opentrends.net>
2017-10-18 14:49:20 +0200

    added spinner logic

--- Filer Reader/Views/Spinner/FRSpinner.swift
+++ Filer Reader/Views/Spinner/FRSpinner.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FRSpinner: UIView {
     private let indicator : UIActivityIndicatorView = UIActivityIndicatorView()
+    private let backgroundView : UIView = UIView()
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -20,12 +21,29 @@ class FRSpinner: UIView {
     init() {
         let screenBounds : CGRect = UIScreen.main.bounds
         self.frame = CGRect(x: 0, y: 0, width: screenBounds.size.width, height: screenBounds.size.height)
-//        self.indicator.backgroundColor =
+        self.backgroundView.frame = self.frame
+        self.backgroundView.backgroundColor = UIColor.corporateLightBlue
+        self.backgroundView.alpha = 0.5
+        self.addSubview(self.backgroundView)
+        
+        self.indicator.backgroundColor = UIColor.corporateBlue
+        self.indicator.center = self.center
+        self.addSubview(self.indicator)
         
     }
     
+    required init?(coder aDecoder: NSCoder) {
+
+    }
+    
     func start(_ inController: UIViewController) {
-        
+        self.indicator.startAnimating()
+        inController.view.addSubview(self)
+    }
+    
+    func stop() {
+        self.removeFromSuperview()
+        self.indicator.stopAnimating()
     }
 
 }
